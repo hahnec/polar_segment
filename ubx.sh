8d066d6ab51b3e1358cd6d4b505ef043643f3f49
Hahne Christopher (ch21z139) <inbox@christopherhahne.de>
2024-07-26 17:53:01 +0200

    feat(init): initial commit

---
+++ ubx.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --job-name="ps"
+#SBATCH --time=23:00:00
+
+#SBATCH --mail-user=christopher.hahne@unibe.ch
+#SBATCH --mail-type=none
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=8G
+#SBATCH --qos=job_gpu
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:rtx3090:1
+
+
+module load Python/3.9.5-GCCcore-10.3.0
+module load CUDA/11.8.0
+
+wandb login --relogin
+
+python -m pip install pip --upgrade
+rm -rf venv
+
+sh ./install.sh
+source ~/33_polar_segment/venv/bin/activate
+
+python -c "import torch; print(torch.cuda.is_available())"
+
+sh ./train.sh
