c4995a33c9382192e17dd28cc08f530239a31d2d
hahnec <christopher.hahne@unibe.ch>
2024-07-26 17:51:36 +0200

    fix(pip): consistent pip usage

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ git clone git@github.com:hahnec/mm_torch
 
 ln -s mm_torch/mm_torch ./mm
 
-python3 -m venv venv
+python -m venv venv
 source venv/bin/activate
 
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
