3a30788bdfd7c19388b2854801f05bcaa06c1fd4
Hahne Christopher (ch21z139) <inbox@christopherhahne.de>
2024-07-27 14:23:46 +0200

    chore(stack): adjust method

--- train_stack.sh
+++ train_stack.sh
@@ -1,18 +1,17 @@
-source venv/bin/activate
 # MLP
-python train.py model=mlp levels=1 kernel_size=0 method=averaging wlens='[550]'
-python train.py model=mlp levels=1 kernel_size=0 method=averaging
-python train.py model=mlp levels=1 kernel_size=3 method=averaging
-python train.py model=mlp levels=1 kernel_size=0 method=pooling
-python train.py model=mlp levels=3 kernel_size=0 method=averaging
-python train.py model=mlp levels=3 kernel_size=3 method=averaging
-python train.py model=mlp levels=3 kernel_size=3 method=averaging activation=leaky
+python train.py model=mlp levels=1 kernel_size=0 wlens='[550]'
+python train.py model=mlp levels=3 kernel_size=0 wlens='[550]'
+python train.py model=mlp levels=3 kernel_size=0 method=averaging wlens='[550]'
+python train.py model=mlp levels=1 kernel_size=0 wlens='[550]'
+python train.py model=mlp levels=1 kernel_size=3 wlens='[550]'
+python train.py model=mlp levels=3 kernel_size=3 wlens='[550]'
+python train.py model=mlp levels=3 kernel_size=3 activation=leaky
 # U-net
 python train.py model=unet levels=1 kernel_size=0
 python train.py model=unet levels=3 kernel_size=0 method=averaging
-python train.py model=unet levels=3 kernel_size=3 method=averaging activation=leaky
+python train.py model=unet levels=3 kernel_size=3 activation=leaky
 python train.py model=unetpp levels=1 kernel_size=0
 # polarimetry data
-python train.py model=unet levels=1 kernel_size=0 method=averaging data_subfolder=polarimetry
-python train.py model=unet levels=1 kernel_size=0 method=averaging data_subfolder=polarimetry wlens='[550]'
-python train.py model=unet levels=1 kernel_size=0 method=averaging data_subfolder=polarimetry_PDDN wlens='[550]'
+python train.py model=unet levels=1 kernel_size=0 data_subfolder=polarimetry
+python train.py model=unet levels=1 kernel_size=0 data_subfolder=polarimetry wlens='[550]'
+python train.py model=unet levels=1 kernel_size=0 data_subfolder=polarimetry_PDDN wlens='[550]'
