ac61e929df49fd9ee8ed03b9307b1262d113f1b9
hahnec <christopher.hahne@unibe.ch>
2024-07-28 22:51:33 +0200

    fix(th): consider batched threshold dimension

--- utils/draw_segment_img.py
+++ utils/draw_segment_img.py
@@ -4,7 +4,7 @@ from torchvision.utils import draw_segmentation_masks
 
 def draw_segmentation_imgs(imgs, preds, truth, bidx=0, th=None, alpha=0.5):
 
-    th = [0.5,] * 2 if th is None else th
+    th = [0.5,] * 2 if th is None else th[0]
     img = (imgs[bidx, 0][None].repeat(3, 1, 1)/imgs[bidx, 0].max()*255).cpu().to(torch.uint8)
     combined_masks = torch.stack((preds[bidx, -1]>th[-1], preds[bidx, -2]>th[-2], truth[bidx, -1]>0, truth[bidx, -2]>0)).cpu()
     frame_pred = draw_segmentation_masks(img, masks=combined_masks[:2], alpha=alpha, colors=['blue', 'orange'])
