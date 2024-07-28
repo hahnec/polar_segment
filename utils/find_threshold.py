7803f88538916c1dfe82f9fa76de555ea272d7dc
hahnec <christopher.hahne@unibe.ch>
2024-07-28 20:00:41 +0200

    fix(nth): catch borderline cases

--- utils/find_threshold.py
+++ utils/find_threshold.py
@@ -21,6 +21,9 @@ def find_optimal_threshold(predictions, ground_truth, num_classes, thresholds=np
         gmeans = (tpr * (1-fpr))**.5
         th_idx = np.argmax(gmeans)
         best_threshold = thresholds[th_idx]
+
+        # catch borderline cases
+        if np.isinf(best_threshold) or np.isnan(best_threshold) or best_threshold == 0: best_threshold = 0.5
         
         optimal_thresholds.append(best_threshold)
 
