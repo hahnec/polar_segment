0c19db22481d2041d51f7f4ed884fe2ea7d78a4a
Hahne Christopher (ch21z139) <inbox@christopherhahne.de>
2024-07-29 22:53:31 +0200

    fix(mem): lower memory footprint for patchresnet

--- train_stack.sh
+++ train_stack.sh
@@ -20,5 +20,5 @@ python train.py group=$1 model=unet levels=1 kernel_size=0 data_subfolder=polari
 python train.py group=$1 model=mlp lr=1e-3 levels=1 kernel_size=0 data_subfolder=polarimetry_PDDN
 python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=0 data_subfolder=polarimetry_PDDN
 # resnet
-python train.py group=$1 model=resnet lr=1e-5 levels=1 kernel_size=0 crop=50
-python train.py group=$1 model=resnet lr=1e-5 levels=1 kernel_size=0 crop=50 data_subfolder=polarimetry_PDDN
+python train.py group=$1 model=resnet batch_size=1 lr=1e-5 levels=1 kernel_size=0 crop=50
+python train.py group=$1 model=resnet batch_size=1 lr=1e-5 levels=1 kernel_size=0 crop=50 data_subfolder=polarimetry_PDDN
