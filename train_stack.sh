d28503b537ac6f0ed230c33feaf489e00825d660
Hahne Christopher (ch21z139) <inbox@christopherhahne.de>
2024-07-29 11:59:09 +0200

    chore(cfg): amend config

--- train_stack.sh
+++ train_stack.sh
@@ -5,13 +5,18 @@ python train.py model=mlp levels=3 kernel_size=0 method=averaging wlens='[550]'
 python train.py model=mlp levels=1 kernel_size=0 wlens='[550]'
 python train.py model=mlp levels=1 kernel_size=3 wlens='[550]'
 python train.py model=mlp levels=3 kernel_size=3 wlens='[550]'
-python train.py model=mlp levels=3 kernel_size=3 activation=leaky
+python train.py model=mlp levels=3 kernel_size=3 activation=leaky wlens='[550]'
+python train.py model=mlp levels=5 kernel_size=3 wlens='[550]'
+python train.py model=mlp levels=5 kernel_size=0
+python train.py model=mlp levels=5 kernel_size=3
 # U-net
 python train.py model=unet levels=1 kernel_size=0
 python train.py model=unet levels=3 kernel_size=0 method=averaging
 python train.py model=unet levels=3 kernel_size=3 activation=leaky
-python train.py model=unetpp levels=1 kernel_size=0
+python train.py model=unetpp levels=1 kernel_size=0 crop=224
 # polarimetry data
 python train.py model=unet levels=1 kernel_size=0 data_subfolder=polarimetry wlens='[550]'
 python train.py model=unet levels=1 kernel_size=0 data_subfolder=polarimetry wlens='[550]'
 python train.py model=unet levels=1 kernel_size=0 data_subfolder=polarimetry_PDDN wlens='[550]'
+python train.py model=mlp levels=1 kernel_size=0 data_subfolder=polarimetry_PDDN wlens='[550]'
+python train.py model=mlp levels=3 kernel_size=0 data_subfolder=polarimetry_PDDN wlens='[550]'
