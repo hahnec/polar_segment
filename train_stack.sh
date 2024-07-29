69fde5dd9796e354b952b4cf1905785dd6a5ed0d
hahnec <christopher.hahne@unibe.ch>
2024-07-29 12:00:38 +0200

    fix(cfg): use mlp with lower learning rate

--- train_stack.sh
+++ train_stack.sh
@@ -1,14 +1,14 @@
 # MLP
-python train.py model=mlp levels=1 kernel_size=0 wlens='[550]'
-python train.py model=mlp levels=3 kernel_size=0 wlens='[550]'
-python train.py model=mlp levels=3 kernel_size=0 method=averaging wlens='[550]'
-python train.py model=mlp levels=1 kernel_size=0 wlens='[550]'
-python train.py model=mlp levels=1 kernel_size=3 wlens='[550]'
-python train.py model=mlp levels=3 kernel_size=3 wlens='[550]'
-python train.py model=mlp levels=3 kernel_size=3 activation=leaky wlens='[550]'
-python train.py model=mlp levels=5 kernel_size=3 wlens='[550]'
-python train.py model=mlp levels=5 kernel_size=0
-python train.py model=mlp levels=5 kernel_size=3
+python train.py model=mlp lr=1e-3 levels=1 kernel_size=0 wlens='[550]'
+python train.py model=mlp lr=1e-3 levels=3 kernel_size=0 wlens='[550]'
+python train.py model=mlp lr=1e-3 levels=3 kernel_size=0 method=averaging wlens='[550]'
+python train.py model=mlp lr=1e-3 levels=1 kernel_size=0 wlens='[550]'
+python train.py model=mlp lr=1e-3 levels=1 kernel_size=3 wlens='[550]'
+python train.py model=mlp lr=1e-3 levels=3 kernel_size=3 wlens='[550]'
+python train.py model=mlp lr=1e-3 levels=3 kernel_size=3 activation=leaky wlens='[550]'
+python train.py model=mlp lr=1e-3 levels=5 kernel_size=3 wlens='[550]'
+python train.py model=mlp lr=1e-3 levels=5 kernel_size=0
+python train.py model=mlp lr=1e-3 levels=5 kernel_size=3
 # U-net
 python train.py model=unet levels=1 kernel_size=0
 python train.py model=unet levels=3 kernel_size=0 method=averaging
@@ -18,5 +18,5 @@ python train.py model=unetpp levels=1 kernel_size=0 crop=224
 python train.py model=unet levels=1 kernel_size=0 data_subfolder=polarimetry wlens='[550]'
 python train.py model=unet levels=1 kernel_size=0 data_subfolder=polarimetry wlens='[550]'
 python train.py model=unet levels=1 kernel_size=0 data_subfolder=polarimetry_PDDN wlens='[550]'
-python train.py model=mlp levels=1 kernel_size=0 data_subfolder=polarimetry_PDDN wlens='[550]'
-python train.py model=mlp levels=3 kernel_size=0 data_subfolder=polarimetry_PDDN wlens='[550]'
+python train.py model=mlp lr=1e-3 levels=1 kernel_size=0 data_subfolder=polarimetry_PDDN wlens='[550]'
+python train.py model=mlp lr=1e-3 levels=3 kernel_size=0 data_subfolder=polarimetry_PDDN wlens='[550]'
