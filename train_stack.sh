6cb26bbfab65e41d0a468a74632de00b3197f20b
hahnec <christopher.hahne@unibe.ch>
2024-07-29 23:05:31 +0200

    chore(act): use ReLU for comparison

--- train_stack.sh
+++ train_stack.sh
@@ -5,13 +5,13 @@ python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=0 method=averagi
 python train.py group=$1 model=mlp lr=1e-3 levels=1 kernel_size=0
 python train.py group=$1 model=mlp lr=1e-3 levels=1 kernel_size=3
 python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=3
-python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=3 activation=leaky
+python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=3 activation=True
 python train.py group=$1 model=mlp lr=1e-3 levels=5 kernel_size=3
 python train.py group=$1 model=mlp lr=1e-3 levels=5 kernel_size=0
 # U-net
 python train.py group=$1 model=unet levels=1 kernel_size=0
 python train.py group=$1 model=unet levels=3 kernel_size=0 method=averaging
-python train.py group=$1 model=unet levels=3 kernel_size=3 activation=leaky
+python train.py group=$1 model=unet levels=3 kernel_size=3 activation=True
 python train.py group=$1 model=unetpp levels=1 kernel_size=0 crop=224
 # polarimetry
 python train.py group=$1 model=unet levels=1 kernel_size=0 data_subfolder=polarimetry
