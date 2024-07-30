fix(bg): rectify GT pixel masking

train.py
@@ -56,8 +56,8 @@ def batch_iter(frames, truth, cfg, model, train_opt=0, th=None, criterion=None, 
         t_s = time.perf_counter() - t_s
         if cfg.labeled_only:
             # skip unlabeled pixels
-            m = torch.any(truth, dim=1, keepdim=True).repeat(1, preds.shape[1], 1, 1).bool()
-            preds, truth = preds[m], truth[m]
+            m = torch.any(truth, dim=1, keepdim=True).squeeze().bool()
+            preds, truth = preds[..., m], truth[..., m]
         loss = criterion(preds, truth) if criterion else None
 
     if train_opt:
@@ -76,8 +76,8 @@ def batch_iter(frames, truth, cfg, model, train_opt=0, th=None, criterion=None, 
 
     # metrics
     th = 0.5 if th is None else th
-    dice = compute_generalized_dice(preds>th, truth, include_background=truth.shape[1]==3)
-    iou = compute_iou(preds>th, truth, include_background=truth.shape[1]==3, ignore_empty=False)
+    dice = compute_generalized_dice(preds>th, truth, include_background=cfg.bg_opt)
+    iou = compute_iou(preds>th, truth, include_background=cfg.bg_opt, ignore_empty=False)
     metrics = {'dice': dice, 'iou': iou, 't_s': torch.tensor([t_s])}
 
     return loss, preds, metrics, imgs