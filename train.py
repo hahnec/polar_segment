6fdb3dcbc876f05187cca2dbf86a50a3912b66bb
hahnec <christopher.hahne@unibe.ch>
2024-07-31 23:15:30 +0200

    fix(label): skip label selection for training

--- train.py
+++ train.py
@@ -44,8 +44,8 @@ def batch_iter(frames, truth, cfg, model, train_opt=0, th=None, criterion=None, 
         imgs = frames[:, p_indices]
         frames = frames[:, n_indices]
 
-    # skip unlabeled pixels
-    m = torch.any(truth, dim=1, keepdim=True).repeat(1, truth.shape[1], 1, 1) if cfg.labeled_only else torch.ones_like(truth)
+    # initialize label selection
+    m = torch.any(truth, dim=1, keepdim=True).repeat(1, truth.shape[1], 1, 1) if False else torch.ones_like(truth)
 
     if cfg.data_subfolder.__contains__('raw') and 'mask' in cfg.feature_keys:
         # remove the feasibility mask from the features
