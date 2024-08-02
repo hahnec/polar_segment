0e4c8340e724175d6945222f9ef03faefbe09b8f
Hahne Christopher (ch21z139) <inbox@christopherhahne.de>
2024-08-03 00:42:38 +0200

    fix(act): use correct activation arg

--- train_stack.sh
+++ train_stack.sh
@@ -4,7 +4,7 @@ python train.py group=$1 model=resnet lr=1e-5 levels=1 kernel_size=0 data_subfol
 # U-net
 python train.py group=$1 model=unet levels=1 kernel_size=0
 python train.py group=$1 model=unet levels=3 kernel_size=0 method=averaging
-python train.py group=$1 model=unet levels=3 kernel_size=3 activation=True
+python train.py group=$1 model=unet levels=3 kernel_size=3 activation=leaky
 python train.py group=$1 model=unetpp levels=1 kernel_size=0 crop=224
 # polarimetry
 python train.py group=$1 model=unet levels=1 kernel_size=0 data_subfolder=polarimetry
@@ -18,5 +18,5 @@ python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=0 method=averagi
 python train.py group=$1 model=mlp lr=1e-3 levels=1 kernel_size=0
 python train.py group=$1 model=mlp lr=1e-3 levels=1 kernel_size=3
 python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=3
-python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=3 activation=True
+python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=3 activation=leaky
 
