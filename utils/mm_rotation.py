fix(sign): rectify sign in angles

utils/mm_rotation.py
@@ -6,7 +6,7 @@ import torchvision.transforms.functional as F
 
 
 class RandomMuellerRotation(object):
-    """Rotate the image by angle.
+    """Rotate the Mueller matrix frame by angle.
 
     Args:
         degrees (sequence or float or int): Range of degrees to select from.
@@ -90,7 +90,7 @@ class RandomMuellerRotation(object):
             rotated_img = F.rotate(img, angle, self.resample, self.expand, self.center, self.fill)
             rotated_img = rotated_img.permute(0, 2, 3, 1).unsqueeze(1)
             # mueller matrix transformation
-            rotated_img = self.get_rmat(-angle) @ rotated_img.view(*rotated_img.shape[:-1], 4, 4) @ self.get_rmat(angle)
+            rotated_img = self.get_rmat(angle) @ rotated_img.view(*rotated_img.shape[:-1], 4, 4) @ self.get_rmat(-angle)
             rotated_img = rotated_img.flatten(-2, -1)
             if label is not None:
                 label = label[:, 0].permute(0, 3, 1, 2)
@@ -114,7 +114,7 @@ class RandomMuellerRotation(object):
     
 
 class RawRandomMuellerRotation(object):
-    """Rotate the image by angle.
+    """Rotate the raw polarimetry instrument data by angle.
 
     Args:
         degrees (sequence or float or int): Range of degrees to select from.
@@ -202,8 +202,8 @@ class RawRandomMuellerRotation(object):
             I, A, W = [el.reshape(shape) for el in [I, A, W]]
             if transpose: I, A, W = [el.transpose(-2, -1) for el in [I, A, W]]
             # mueller matrix transformation: A_theta = (R_theta @ A_inv)_inv since R_theta @ M @ R_-theta = R_theta @ A_inv @ I @ W_inv @ R_-theta
-            A = torch.linalg.inv(self.get_rmat(-angle) @ torch.linalg.inv(A))
-            W = torch.linalg.inv(torch.linalg.inv(W) @ self.get_rmat(angle))
+            A = torch.linalg.inv(self.get_rmat(angle) @ torch.linalg.inv(A))
+            W = torch.linalg.inv(torch.linalg.inv(W) @ self.get_rmat(-angle))
             # HxWx4 to HxWx16 matrix reshaping
             if transpose: I, A, W = [el.transpose(-2, -1) for el in [I, A, W]]
             I, A, W = [el.flatten(-2, -1).moveaxis(-1, 0) for el in [I, A, W]]