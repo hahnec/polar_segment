3a7470a15784074a4d65dde8443ed46e051b527b
hahnec <christopher.hahne@unibe.ch>
2024-08-04 15:48:45 +0200

    fix(dims): consider new image input dimensions

--- utils/draw_segment_img.py
+++ utils/draw_segment_img.py
@@ -9,7 +9,7 @@ def draw_segmentation_imgs(imgs, preds, truth, bidx=0, th=None, alpha=0.5):
         combined_masks = torch.stack((preds_b[bidx, -1], preds_b[bidx, -2], truth[bidx, -1]>0, truth[bidx, -2]>0)).cpu()
     else:
         combined_masks = torch.stack((preds[bidx, -1]>th[bidx][-1], preds[bidx, -2]>th[bidx][-2], truth[bidx, -1]>0, truth[bidx, -2]>0)).cpu()
-    img = (imgs[bidx, 0][None].repeat(3, 1, 1)/imgs[bidx, 0].max()*255).cpu().to(torch.uint8)
+    img = (imgs[bidx][None].repeat(3, 1, 1)/imgs[bidx].max()*255).cpu().to(torch.uint8)
     frame_pred = draw_segmentation_masks(img, masks=combined_masks[:2], alpha=alpha, colors=['orange', 'blue'])
     frame_mask = draw_segmentation_masks(img, masks=combined_masks[2:], alpha=alpha, colors=['red', 'green'])
 
