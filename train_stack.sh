8e07f8c06e885182d3cf3c03061f79986bb529bd
hahnec <christopher.hahne@unibe.ch>
2024-08-07 20:35:04 +0200

    chore(runs): skip less promising runs

--- train_stack.sh
+++ train_stack.sh
@@ -6,16 +6,15 @@ python train.py group=$1 model=unet levels=1 kernel_size=0
 python train.py group=$1 model=unet levels=3 kernel_size=0 method=averaging
 python train.py group=$1 model=unet levels=3 kernel_size=3 activation=leaky
 python train.py group=$1 model=unetpp levels=1 kernel_size=0 crop=224
-# polarimetry
 python train.py group=$1 model=unet levels=1 kernel_size=0 data_subfolder=polarimetry
 python train.py group=$1 model=unet levels=1 kernel_size=0 data_subfolder=polarimetry_PDDN
-python train.py group=$1 model=mlp lr=1e-3 levels=1 kernel_size=0 data_subfolder=polarimetry_PDDN
-python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=0 data_subfolder=polarimetry_PDDN
 # MLP
 python train.py group=$1 model=mlp lr=1e-3 levels=1 kernel_size=0
-python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=0
+#python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=0
 #python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=0 method=averaging
-python train.py group=$1 model=mlp lr=1e-3 levels=1 kernel_size=3
-python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=3
-#python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=3 activation=leaky
+#python train.py group=$1 model=mlp lr=1e-3 levels=1 kernel_size=3
+#python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=3
+python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=3 activation=leaky
+python train.py group=$1 model=mlp lr=1e-3 levels=1 kernel_size=0 data_subfolder=polarimetry_PDDN
+#python train.py group=$1 model=mlp lr=1e-3 levels=3 kernel_size=0 data_subfolder=polarimetry_PDDN
 
