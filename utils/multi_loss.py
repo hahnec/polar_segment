8486ca029c7448355c90cb837b886b95018ba2b5
hahnec <christopher.hahne@unibe.ch>
2024-08-07 17:52:41 +0200

    refactor(split): split functions to allow for reuse

--- utils/multi_loss.py
+++ utils/multi_loss.py
@@ -1,12 +1,35 @@
 import torch
 
+
+def reduce_ht(x, y):
+
+    h_pred = torch.mean(torch.stack([x[:, -3], x[:, -4]], dim=0), dim=0)
+    t_pred = torch.mean(torch.stack([x[:, -1], x[:, -2]], dim=0), dim=0)
+    h_true = torch.logical_or(y[:, -3], y[:, -4]).float()
+    t_true = torch.logical_or(y[:, -1], y[:, -2]).float()
+
+    return h_pred, t_pred, h_true, t_true
+
+
+def reduce_wg(x, y):
+
+    w_pred = torch.mean(torch.stack([x[:, -2], x[:, -4]], dim=0), dim=0)
+    g_pred = torch.mean(torch.stack([x[:, -1], x[:, -3]], dim=0), dim=0)
+    w_true = torch.logical_or(y[:, -2], y[:, -4]).float()
+    g_true = torch.logical_or(y[:, -1], y[:, -3]).float()
+
+    return w_pred, g_pred, w_true, g_true
+
+
 def multi_loss_aggregation(x, y, loss_fun, w_lambda=None):
 
     a_loss = loss_fun(x, y)
-    t_loss = loss_fun(torch.mean(torch.stack([x[:, -1], x[:, -2]], dim=0), dim=0), torch.logical_or(y[:, -1], y[:, -2]).float())
-    h_loss = loss_fun(torch.mean(torch.stack([x[:, -3], x[:, -4]], dim=0), dim=0), torch.logical_or(y[:, -3], y[:, -4]).float())
-    w_loss = loss_fun(torch.mean(torch.stack([x[:, -2], x[:, -4]], dim=0), dim=0), torch.logical_or(y[:, -2], y[:, -4]).float())
-    g_loss = loss_fun(torch.mean(torch.stack([x[:, -1], x[:, -3]], dim=0), dim=0), torch.logical_or(y[:, -1], y[:, -3]).float())
+    h_pred, t_pred, h_true, t_true = reduce_ht(x, y)
+    w_pred, g_pred, w_true, g_true = reduce_wg(x, y)
+    h_loss = loss_fun(h_pred, h_true)
+    t_loss = loss_fun(t_pred, t_true)
+    w_loss = loss_fun(w_pred, w_true)
+    g_loss = loss_fun(g_pred, g_true)
 
     if w_lambda is None: w_lambda = torch.tensor([x.shape[1], 1, 1, 1, 1], device=x.device, dtype=x.dtype)
 
