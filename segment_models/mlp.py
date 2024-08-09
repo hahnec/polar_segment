4bb7dd9c713a060a899a7bfee73c7fc9a107673c
hahnec <christopher.hahne@unibe.ch>
2024-08-09 13:27:32 +0200

    feat(mlp): add more functionality to MLP such as dropout, GELU and weights init

--- segment_models/mlp.py
+++ segment_models/mlp.py
@@ -7,15 +7,26 @@ class MLP(torch.nn.Module):
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.hidden_layer = torch.nn.Linear(n_channels, hidden_dim)  # Single hidden layer with 64 units
-        self.relu = torch.nn.ReLU()  # ReLU activation function
+        self.act_fun = torch.nn.ReLU() if True else torch.nn.GELU()  # activation function
         self.output_layer = torch.nn.Linear(hidden_dim, n_classes)  # Output layer
+        self.dropout = torch.nn.Dropout(.5)
+
+        self._init_weights()
+
+    def _init_weights(self):
+        torch.nn.init.xavier_uniform_(self.hidden_layer.weight)
+        torch.nn.init.xavier_uniform_(self.output_layer.weight)
+        torch.nn.init.normal_(self.hidden_layer.bias, std=1e-6)
+        torch.nn.init.normal_(self.output_layer.bias, std=1e-6)
 
     def forward(self, x):
         dims = x.shape
         x = x.permute(0, 2, 3, 1).reshape(-1, self.n_channels) if len(dims) == 4 else x
         x = self.hidden_layer(x)
-        x = self.relu(x)
+        x = self.act_fun(x)
+        x = self.dropout(x)
         x = self.output_layer(x)
+        x = self.dropout(x)
         x = torch.nn.functional.softmax(x, dim=1)
         return x.view(dims[0], *dims[2:], self.n_classes).permute(0, 3, 1, 2) if len(dims) == 4 else x
 
