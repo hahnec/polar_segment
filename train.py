b3695f9ead181eff18f6a05222268bd3585d273f
hahnec <christopher.hahne@unibe.ch>
2024-08-11 01:17:02 +0200

    feat(loss): use focal loss

--- train.py
+++ train.py
@@ -87,10 +87,11 @@ def batch_iter(frames, truth, cfg, model, train_opt=0, criterion=None, optimizer
 
 def epoch_iter(cfg, dataloader, model, mm_model=None, branch_type='test', step=None, log_img=False, epoch=None, optimizer=None, grad_scaler=None):
 
-    criterion = WeightedDiceBCE(dice_weight=0.5, BCE_weight=0.5) if branch_type != 'test' else None
+    from torchvision.ops import sigmoid_focal_loss
+    criterion = sigmoid_focal_loss if branch_type != 'test' else None
     if cfg.class_num > 3 and branch_type != 'test':
         from utils.multi_loss import multi_loss_aggregation
-        criterion = lambda x, y: multi_loss_aggregation(x, y, loss_fun=WeightedDiceBCE(dice_weight=0.5, BCE_weight=0.5))
+        criterion = lambda x, y: multi_loss_aggregation(x, y, loss_fun=sigmoid_focal_loss)
     train_opt = 0 if optimizer is None else 1
     model.train() if train_opt else model.eval()
     batch_it = lambda f, t: batch_iter(f, t, cfg=cfg, model=model, train_opt=train_opt, criterion=criterion, optimizer=optimizer, grad_scaler=grad_scaler)
