7b97e16c4525d23b344ec0bdce940b5f7a8838c1
hahnec <christopher.hahne@unibe.ch>
2024-08-11 09:58:22 +0200

    fix(loss): compute scalar loss

--- train.py
+++ train.py
@@ -88,10 +88,10 @@ def batch_iter(frames, truth, cfg, model, train_opt=0, criterion=None, optimizer
 def epoch_iter(cfg, dataloader, model, mm_model=None, branch_type='test', step=None, log_img=False, epoch=None, optimizer=None, grad_scaler=None):
 
     from torchvision.ops import sigmoid_focal_loss
-    criterion = sigmoid_focal_loss if branch_type != 'test' else None
+    criterion = lambda x, y: sigmoid_focal_loss(x, y).mean() if branch_type != 'test' else None
     if cfg.class_num > 3 and branch_type != 'test':
         from utils.multi_loss import multi_loss_aggregation
-        criterion = lambda x, y: multi_loss_aggregation(x, y, loss_fun=sigmoid_focal_loss)
+        criterion = lambda x, y: multi_loss_aggregation(x, y, loss_fun=lambda x, y: sigmoid_focal_loss(x, y).mean())
     train_opt = 0 if optimizer is None else 1
     model.train() if train_opt else model.eval()
     batch_it = lambda f, t: batch_iter(f, t, cfg=cfg, model=model, train_opt=train_opt, criterion=criterion, optimizer=optimizer, grad_scaler=grad_scaler)
