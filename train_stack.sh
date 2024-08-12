75284ea986e9bdcfc6c46f0e2b39912386159406
hahnec <christopher.hahne@unibe.ch>
2024-08-12 21:48:56 +0200

    feat(raw): use direct mueller matrix feeding

--- train_stack.sh
+++ train_stack.sh
@@ -3,6 +3,7 @@ python train.py group=$1 model=resnet lr=1e-5 levels=1 kernel_size=0
 python train.py group=$1 model=resnet lr=1e-5 levels=1 kernel_size=0 data_subfolder=polarimetry_PDDN 
 # U-net
 python train.py group=$1 model=unet levels=1 kernel_size=0
+python train.py group=$1 model=unet levels=0 kernel_size=0  # select 10 relevant Mueller matrix channels
 python train.py group=$1 model=unet levels=3 kernel_size=0 method=averaging
 python train.py group=$1 model=unet levels=3 kernel_size=3 activation=leaky
 python train.py group=$1 model=unetpp levels=1 kernel_size=0
