9701af042ef7c9ff3c9f86c47bf01ff0d1569dc3
hahnec <christopher.hahne@unibe.ch>
2024-08-13 19:45:58 +0200

    feat(unetpp): cmbine unetpp with raw channel selector

--- train_stack.sh
+++ train_stack.sh
@@ -7,6 +7,7 @@ python train.py group=$1 model=unet levels=0 kernel_size=0  # select 10 relevant
 python train.py group=$1 model=unet levels=3 kernel_size=0 method=averaging
 python train.py group=$1 model=unet levels=3 kernel_size=3 activation=leaky
 python train.py group=$1 model=unetpp levels=1 kernel_size=0
+python train.py group=$1 model=unetpp levels=0 kernel_size=0 # select 10 relevant Mueller matrix channels
 python train.py group=$1 model=uctransnet levels=1 kernel_size=0 crop=224
 python train.py group=$1 model=unet levels=1 kernel_size=0 data_subfolder=polarimetry
 python train.py group=$1 model=unet levels=1 kernel_size=0 data_subfolder=polarimetry_PDDN
