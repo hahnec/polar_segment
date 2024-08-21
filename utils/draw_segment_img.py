c03acc599a3b106ccf355a8df242eae2ce175564
hahnec <christopher.hahne@unibe.ch>
2024-08-21 09:18:28 +0200

    style(color): swap gray for yellow

--- utils/draw_segment_img.py
+++ utils/draw_segment_img.py
@@ -5,7 +5,7 @@ from torchvision.utils import draw_segmentation_masks
 def draw_segmentation_imgs(imgs, preds, truth, bidx=0, bg_opt=False, alpha=0.3):
 
     n_channels = truth.shape[1]
-    colors = ['gray', 'green', 'red', 'blue'] if n_channels-int(bg_opt) > 2 else ['gray', 'green', 'red'] 
+    colors = ['yellow', 'green', 'red', 'blue'] if n_channels-int(bg_opt) > 2 else ['yellow', 'green', 'red'] 
     preds_b = torch.nn.functional.one_hot(preds.argmax(1), num_classes=truth.shape[1]).permute(0, 3, 1, 2).bool()
     combined_masks = torch.stack((preds_b[bidx], truth[bidx]>0)).cpu()
     img = (imgs[bidx][None].repeat(3, 1, 1)/imgs[bidx].max()*255).cpu().to(torch.uint8)
