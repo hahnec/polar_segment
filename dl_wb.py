ccc31e4dd05767d57b4f63efee6e6e9a7645cb9f
hahnec <christopher.hahne@unibe.ch>
2024-09-04 17:21:46 +0200

    feat(plots): add capability to download plot data

--- dl_wb.py
+++ dl_wb.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     wandb.login(key=cfg.wandb_api)
 
     # Define the project and group name
-    group_name = 'htgm_locvar8'
+    group_name = 'kfold_100epochs'
     project_name = 'polar_segment'
     table_key = 'report'
     media_keys = ['heatmap_test', 'img_pred_test', 'img_mask_test']
@@ -83,32 +83,62 @@ if __name__ == "__main__":
         metrics = {}
         for col in ['accuracy', 'dice', 'iou', 'auc', 't_mm', 't_s']:
             metrics[col] = run.summary.get(col)
-        with open(os.path.join(group_name, 'metrics_%s.json' % str(run.name)), 'w') as file:
-            json.dump(metrics, file, indent=4)
-        md_metrics = dict_to_markdown_table(metrics)
-        with open(os.path.join(group_name, 'metrics_%s.md' % str(run.name)), 'w') as file:
-            file.write(md_metrics)
+        with open(os.path.join(group_name, 'metrics_%s.json' % str(run.name)), 'w') as f:
+            json.dump(metrics, f, indent=4)
+        #md_metrics = dict_to_markdown_table(metrics)
+        #with open(os.path.join(group_name, 'metrics_%s.md' % str(run.name)), 'w') as f:
+        #    f.write(md_metrics)
 
         table = run.use_artifact(run.logged_artifacts()[1]).get(table_key)
         table_dict = {
             row[0]: {table.columns[i]: row[i] for i in range(1, len(table.columns))}
             for row in table.data
         }
-        with open(os.path.join(group_name, 'table_%s.json' % str(run.name)), 'w') as file:
-            json.dump(table_dict, file, indent=4)
+        with open(os.path.join(group_name, 'table_%s.json' % str(run.name)), 'w') as f:
+            json.dump(table_dict, f, indent=4)
         md_table = convert_to_markdown(table)
-        with open(os.path.join(group_name, 'table_%s.md' % str(run.name)), 'w') as file:
-            file.write(md_table)
+        with open(os.path.join(group_name, 'table_%s.md' % str(run.name)), 'w') as f:
+            f.write(md_table)
 
         # images
+        captions = {}
+        base_url = 'https://api.wandb.ai/files/hahnec/' + project_name + '/' + run.url.split('runs/')[-1] + '/'
         for media_key in media_keys:
             media_files = run.history(keys=[media_key]).get(media_key, [])
             for i, media in enumerate(media_files):
                 if isinstance(media, dict) and 'path' in media:
-                    image_url = 'https://api.wandb.ai/files/hahnec/' + project_name + '/' + run.url.split('runs/')[-1] + '/' + media['path']
+                    image_url = base_url + media['path']
                     base_name = '_'.join(os.path.basename(image_url).split('_')[:-1])+'.png' # skip hash clutter
                     image_name = f"{run.name}_{i}_{base_name}"
                     save_path = os.path.join(output_dir, image_name)
                     download_image(image_url, save_path)
+
+                    # store captions containing labels
+                    fname = save_path.split('/')[-1]
+                    captions[fname] = media['caption']
+        import yaml
+        with open(os.path.join(output_dir, 'captions.yml'), 'w') as f:
+            yaml.dump(captions, f)
+
+        # download ROC plot data
+        artifact_dict = run.history(keys=['roc_table']).get('roc_table', [])[0]
+        roc_url = base_url + artifact_dict['path']
+        response = requests.get(roc_url)
+        if response.status_code == 200:
+            content_str = response.content.decode('utf-8')
+            roc_dict = json.loads(content_str)
+            with open(os.path.join(group_name, 'roc_%s.json' % str(run.name)), 'w') as f:
+                json.dump(roc_dict, f)
+
+        # download training and validation curves
+        curves_dict = {
+            'train_loss': None, 'valid_loss': None, 
+            'train_dice': None, 'valid_dice': None,
+            'train_acc': None, 'valid_acc': None,
+            }
+        for k in curves_dict.keys():
+            curves_dict[k] = [r[k] for _, r in run.history(keys=[k]).iterrows()]
+        with open(os.path.join(group_name, 'curves_%s.json' % str(run.name)), 'w') as f:
+            json.dump(curves_dict, f)
 
     print("Done.")
