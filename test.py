41464bc6f749ccd6c6203d0a5392e74e19ccc9c7
hahnec <christopher.hahne@unibe.ch>
2024-09-06 16:52:32 +0200

    chore(test): bring tst script up-to-date

--- test.py
+++ test.py
@@ -89,7 +89,7 @@ def flatten_dict_to_rows(d):
 if __name__ == '__main__':
 
     # load configuration
-    cfg = OmegaConf.load('./configs/test.yml')
+    cfg = OmegaConf.load('./configs/train_local.yml')
 
     # override loaded configuration with test config
     cfg = OmegaConf.merge(cfg, OmegaConf.load('./configs/test.yml'))
@@ -120,36 +120,36 @@ if __name__ == '__main__':
         mm_model = None
 
     # create dataset
-    dataset = HORAO(cfg.data_dir, 'test.txt', transforms=[ToTensor()], bg_opt=cfg.bg_opt, data_subfolder=cfg.data_subfolder, keys=cfg.feature_keys, wlens=cfg.wlens)
+    dataset = HORAO(cfg.data_dir, ['test.txt'], transforms=[ToTensor()], bg_opt=cfg.bg_opt, data_subfolder=cfg.data_subfolder, keys=cfg.feature_keys, wlens=cfg.wlens)
 
     # model selection
-    n_channels = mm_model.ochs if cfg.data_subfolder.__contains__('raw') else len([k for k in dataset.keys if k != 'intensity'])
+    n_channels = mm_model.ochs if cfg.data_subfolder.__contains__('raw') else len(cfg.feature_keys)
     if cfg.model == 'mlp':
         from segment_models.mlp import MLP
-        model = MLP(n_channels=n_channels, n_classes=2+dataset.bg_opt,)
+        model = MLP(n_channels=n_channels, n_classes=cfg.class_num+cfg.bg_opt)
     elif cfg.model == 'resnet':
         from horao_dataset import PatchHORAO as HORAO
         from segment_models.resnet import PatchResNet
-        model = PatchResNet(n_channels=n_channels, n_classes=2+cfg.bg_opt, patch_size=50, testing=True)
+        model = PatchResNet(n_channels=n_channels, n_classes=cfg.class_num+cfg.bg_opt, patch_size=50)
     elif cfg.model == 'unet':
         from segment_models.unet import UNet
-        model = UNet(n_channels=n_channels, n_classes=2+dataset.bg_opt, shallow=cfg.shallow)
+        model = UNet(n_channels=n_channels, n_classes=cfg.class_num+cfg.bg_opt, shallow=cfg.shallow)
     elif cfg.model == 'unetpp':
-        from segment_models.unet_pp import get_pretrained_unet_pp
-        model = get_pretrained_unet_pp(n_channels, out_channels=2+dataset.bg_opt)
+        from segment_models.unet_pp import UnetPP
+        model = UnetPP(n_channels, out_channels=cfg.class_num+cfg.bg_opt)
     elif cfg.model == 'uctransnet':
         from segment_models.uctransnet.UCTransNet import UCTransNet
         from segment_models.uctransnet.Config import get_CTranS_config
-        model = UCTransNet(n_channels=n_channels, n_classes=2+dataset.bg_opt, in_channels=64, img_size=cfg.crop, config=get_CTranS_config())
+        model = UCTransNet(n_channels=n_channels, n_classes=cfg.class_num+cfg.bg_opt, in_channels=64, img_size=cfg.crop, config=get_CTranS_config())
     else:
-        raise Exception('Model name not recognized')
+        raise Exception('Model %s not recognized' % cfg.model)
 
     model = model.to(memory_format=torch.channels_last)
     model.to(device=cfg.device)
     model.eval()
 
     if cfg.model_file is not None:
-        ckpt_paths = [fn for fn in Path('./ckpts').iterdir() if fn.name.startswith(cfg.model_file.split('_')[0])]
+        ckpt_paths = [fn for fn in Path('./ckpts').iterdir() if fn.name.startswith(cfg.model_file.split('.')[0])]
         state_dict = torch.load(str(ckpt_paths[0]), map_location=cfg.device)
         model.load_state_dict(state_dict) if cfg.model != 'resnet' else model.model.load_state_dict(state_dict)
         logging.info(f'Model loaded from {cfg.model_file}')
@@ -165,6 +165,4 @@ if __name__ == '__main__':
             Device:          {cfg.device}
         ''')
     
-    from utils.find_threshold import get_threshold
-    th = get_threshold(cfg, dataset, model, mm_model) if not cfg.labeled_only else None
-    test_main(cfg, dataset, model, mm_model, th=th)
+    test_main(cfg, dataset, model, mm_model)
