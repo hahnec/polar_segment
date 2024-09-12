574a8da7fd206224a33d2a6b434b03b0313b6451
hahnec <inbox@christopherhahne.de>
2024-09-12 15:49:21 +0200

    fix(plt): improve fiber plot

--- utils/draw_fiber_img.py
+++ utils/draw_fiber_img.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.ndimage as ndimage
 
-def plot_fiber(raw_azimuth, linr, mask=None, intensity=None, n=10):
+def plot_fiber(raw_azimuth, linr, mask=None, intensity=None, n=10, option='quiver'):
 
 	azimuth = np.pi*raw_azimuth/180
 	X, Y = np.meshgrid(np.arange(azimuth.shape[1]), np.arange(azimuth.shape[0]))
@@ -13,8 +13,8 @@ def plot_fiber(raw_azimuth, linr, mask=None, intensity=None, n=10):
 	orientation_sin = np.sin(azimuth_unwrapped)
 
 	window = 5
-	cos_mean = ndimage.uniform_filter(orientation_cos, (window, window))/(window*window)
-	sin_mean = ndimage.uniform_filter(orientation_sin, (window, window))/(window*window)
+	cos_mean = ndimage.uniform_filter(orientation_cos, (window, window))/window**2
+	sin_mean = ndimage.uniform_filter(orientation_sin, (window, window))/window**2
 	magnitude = (cos_mean**2 + sin_mean**2)**.5
 
 	cos_mean_masked = np.ma.array(orientation_cos, mask=mask)
@@ -27,16 +27,32 @@ def plot_fiber(raw_azimuth, linr, mask=None, intensity=None, n=10):
 
 	fig, ax = plt.subplots()
 	if intensity is not None: ax.imshow(intensity, cmap = 'gray')
-	quiver = ax.quiver(X[::n, ::n], Y[::n, ::n], u[::n, ::n], v[::n, ::n],
-            raw_azimuth[::n, ::n],
-            scale = 20, 
-            headwidth=2, 
-            cmap=plt.cm.twilight_shifted,
-            )
+
+	if option == 'quiver':
+		tracts = ax.quiver(
+			X[::n, ::n], Y[::n, ::n], u[::n, ::n], v[::n, ::n],
+			raw_azimuth[::n, ::n],
+			scale = 20, 
+			headwidth = 2, 
+			cmap = plt.cm.twilight_shifted,
+		)
+		fig.colorbar(tracts, orientation='vertical')
+
+	if option == 'streamplot':
+		u = +cos_mean_masked
+		v = -sin_mean_masked
+		tracts = ax.streamplot(
+			X, Y, u, v, color=azimuth_unwrapped, 
+			density = 2, 
+			cmap = plt.cm.twilight_shifted, 
+			#norm = normalizer, 
+			arrowsize = 0, 
+			linewidth = 0.1*linr, 
+			maxlength = 20., 
+		)
+
 	ax.axis('off')
-    
 	plt.tight_layout()
-	fig.colorbar(quiver, orientation='vertical')
 	from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 	canvas = FigureCanvas(fig)
 	canvas.draw()
