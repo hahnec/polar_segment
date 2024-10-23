0c27f805e08a7da5d04d6ba4ecba8d899dfed077
hahnec <inbox@christopherhahne.de>
2024-10-23 17:05:50 +0200

    refactor(plt): rename plot script

--- scripts/plot_learning_curves.py
+++ scripts/plot_learning_curves.py
@@ -20,7 +20,7 @@ def merge_curves(curves_dict, run_type, curve_type='train_loss', smooth_type=1):
         curve_means.append(np.mean(data_arr, axis=0))
         curve_stds.append(np.std(data_arr, axis=0))
 
-    return curve_means, curve_stds
+    return np.array(curve_means), np.array(curve_stds)
 
 def plot_curves(curve_means, curve_stds, labels=None, filename='', fontsize=18):
 
@@ -37,12 +37,15 @@ def plot_curves(curve_means, curve_stds, labels=None, filename='', fontsize=18):
     for mean, std, l, c, s in zip(curve_means, curve_stds, labels, colors, styles):
         axs.plot(x, mean, label=l, color=c, linestyle=s)
         axs.fill_between(x, mean - std, mean + std, color=c, alpha=0.15)
-        axs.set_ylim(0.5, round(max(mean+std), 1)*1.015)
+        if filename.__contains__('loss'):
+            axs.set_ylim(round(np.min(curve_means-curve_stds), 1)*0.985, round(np.max(curve_means+curve_stds), 1)*1.015) 
+        else: 
+            axs.set_ylim(0.5, round(np.max(curve_means+curve_stds), 1)*1.015)
         axs.set_xlim(0, len(x))
     
     axs.set_xlabel('Steps [\\#]', fontsize=fontsize)
-    axs.set_ylabel(filename.split('_')[-1].capitalize()+' score [a.u.]', fontsize=fontsize)
-    axs.legend(loc='lower right', fontsize=fontsize)
+    axs.set_ylabel(filename.split('_')[-1].capitalize()+' [a.u.]', fontsize=fontsize)
+    axs.legend(loc='upper left' if filename.__contains__('loss') else 'lower right', fontsize=fontsize)
     plt.tight_layout()
     plt.savefig(Path(__file__).parent / (filename+'_figure.svg'), format='svg')
 
@@ -58,8 +61,8 @@ def exponential_moving_average(data, alpha=0.3):
 
 if __name__ == "__main__":
 
-    group_names = ['kfold_200epochs_imbalance_ckpt', 'kfold_200epochs_imbalance_ckpt_rotation']#, 'kfold_200epochs_imbalance_ckpt_rotation_spatial'
-    label_names = ['w/o augmentation', 'rotation $\\theta=[-\pi, \pi)$']#, 'flips'
+    group_names = ['kfold_more_healthy_wo_aug', 'kfold_more_healthy_rota', 'kfold_more_healthy_flip', 'kfold_more_healthy_all'] #, 'kfold_200epochs_imbalance_ckpt_rotation_flip'#, 'kfold_200epochs_imbalance_ckpt_rotation_spatial'
+    label_names = ['w/o augmentation', 'rotations $\\theta=[-\pi, \pi)$', 'flips $\mathbf{H}_p$, $\mathbf{V}_p$ and $\mathbf{F}_p$', 'rotations + flips']    #, 'Rotation + Flip'
     curves_dict = {k: {} for k in group_names}
     for group_name in group_names:
         kfold_opt = group_name.lower().translate(str.maketrans('', '', '-_ ')).__contains__('kfold')
@@ -86,7 +89,7 @@ if __name__ == "__main__":
     
     # merge curves
     run_type = 'LCunet'
-    metric_strs = ['train_dice', 'valid_dice']
+    metric_strs = ['train_loss', 'valid_loss']
     for metric_str in metric_strs:
         curve_means, curve_stds = merge_curves(curves_dict, run_type, metric_str)
         plot_curves(curve_means, curve_stds, labels=label_names, filename=run_type+'_'+metric_str)
