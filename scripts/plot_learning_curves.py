77c290d28358308f8313e875d886bf8fcdd31573
Hahne Christopher (ch21z139) <inbox@christopherhahne.de>
2024-10-29 11:28:06 +0100

    style(aug): indicate absence

--- scripts/plot_learning_curves.py
+++ scripts/plot_learning_curves.py
@@ -22,7 +22,7 @@ def merge_curves(curves_dict, run_type, curve_type='train_loss', smooth_type=1):
 
     return np.array(curve_means), np.array(curve_stds)
 
-def plot_curves(curve_means, curve_stds, labels=None, filename='', fontsize=18):
+def plot_curves(curve_means, curve_stds, labels=None, filename='', fontsize=18, semilog_opt=False):
 
     import matplotlib.pyplot as plt
     plt.rcParams['text.usetex'] = True
@@ -32,17 +32,39 @@ def plot_curves(curve_means, curve_stds, labels=None, filename='', fontsize=18):
     fig, axs = plt.subplots(1, 1)
     x = np.arange(len(curve_means[0]))
     colors = ['#d62728', '#1f77b4', '#2ca02c', '#8c564b', '#9467bd', '#ff7f0e'][:len(curve_means)]
-    styles = ['-', '-.', '--', ':'][:len(curve_means)]
+    styles = ['-', '-.', '--', ':', (0, (3, 5, 1, 5, 1, 5)), (5, (10, 3)), (0, (3, 1, 1, 1, 1, 1))][:len(curve_means)]
     labels = [str(num) for num in range(len(curve_means))] if labels is None else labels
     for mean, std, l, c, s in zip(curve_means, curve_stds, labels, colors, styles):
-        axs.plot(x, mean, label=l, color=c, linestyle=s)
+        axs.semilogy(x, mean, label=l, color=c, linestyle=s) if semilog_opt else axs.plot(x, mean, label=l, color=c, linestyle=s)
         axs.fill_between(x, mean - std, mean + std, color=c, alpha=0.15)
         if filename.__contains__('loss'):
-            axs.set_ylim(round(np.min(curve_means-curve_stds), 1)*0.985, round(np.max(curve_means+curve_stds), 1)*1.015) 
-        else: 
-            axs.set_ylim(0.5, round(np.max(curve_means+curve_stds), 1)*1.015)
-        axs.set_xlim(0, len(x))
+            min_ylim = round(np.min(curve_means-curve_stds), 1)*0.985
+            max_ylim = round(np.max(curve_means+0), 1)*1.015
+        else:
+            min_ylim = 0.5
+            max_ylim = round(np.max(curve_means+0), 1)
+        axs.set_ylim(min_ylim, max_ylim)
+        axs.set_xlim(0, len(x)-1)
     
+    if semilog_opt:
+        if True:
+            axs.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{int(x)}"))
+            axs.yaxis.set_minor_formatter(plt.FuncFormatter(lambda x, _: f"{int(x)}"))
+        else:
+            # This clears the ticks
+            axs.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f""))
+            axs.yaxis.set_minor_formatter(plt.FuncFormatter(lambda x, _: f""))
+            axs.set_yticks([])  
+
+            num_ticks = 5
+            step_size = 5
+            custom_ticks = np.linspace((min_ylim//step_size+1)*step_size, (max_ylim//step_size)*step_size, num_ticks)
+
+            axs.set_yticks(custom_ticks)
+
+            axs.set_yticklabels([f"{int(tick)}" for tick in custom_ticks])  # Format as integers
+
+
     axs.set_xlabel('Steps [\\#]', fontsize=fontsize)
     axs.set_ylabel(filename.split('_')[-1].capitalize()+' [a.u.]', fontsize=fontsize)
     axs.legend(loc='upper left' if filename.__contains__('loss') else 'lower right', fontsize=fontsize)
@@ -61,8 +83,9 @@ def exponential_moving_average(data, alpha=0.3):
 
 if __name__ == "__main__":
 
-    group_names = ['kfold_more_healthy_wo_aug', 'kfold_more_healthy_rota', 'kfold_more_healthy_flip', 'kfold_more_healthy_all'] #, 'kfold_200epochs_imbalance_ckpt_rotation_flip'#, 'kfold_200epochs_imbalance_ckpt_rotation_spatial'
-    label_names = ['w/o augmentation', 'rotations $\\theta=[-\pi, \pi)$', 'flips $\mathbf{H}_p$, $\mathbf{V}_p$ and $\mathbf{F}_p$', 'rotations + flips']    #, 'Rotation + Flip'
+    run_type = 'MMFFunet'#'LCunet' #
+    group_names = ['kfold_more_healthy_wo_aug', 'kfold_more_healthy_rota', 'kfold_more_healthy_flip', 'kfold_more_healthy_all', 'kfold_more_healthy_rota_false'] #, 'kfold_200epochs_imbalance_ckpt_rotation_flip'#, 'kfold_200epochs_imbalance_ckpt_rotation_spatial'
+    label_names = ['w/o augmentation', 'rotations $\\theta=[-\pi, \pi)$', 'flips $\mathbf{H}_p$', 'rotations + flips', 'rotation (false)']    #, 'Rotation + Flip'
     curves_dict = {k: {} for k in group_names}
     for group_name in group_names:
         kfold_opt = group_name.lower().translate(str.maketrans('', '', '-_ ')).__contains__('kfold')
@@ -88,7 +111,6 @@ if __name__ == "__main__":
             curves_dict[group_name][method+el[1]].append(curves)
     
     # merge curves
-    run_type = 'LCunet'
     metric_strs = ['train_loss', 'valid_loss']
     for metric_str in metric_strs:
         curve_means, curve_stds = merge_curves(curves_dict, run_type, metric_str)
--- train_augment.sh
+++ train_augment.sh
@@ -2,8 +2,8 @@
 
 for k_select in {0..11}; do
     # U-net
-    python train.py group=$1 model=unet levels=1 kernel_size=0 k_select=$k_select
-    python train.py group=$1 model=unet levels=0 kernel_size=0 k_select=$k_select
+    python train.py group=$1_absence model=unet levels=1 kernel_size=0 k_select=$k_select
+    python train.py group=$1_absence model=unet levels=0 kernel_size=0 k_select=$k_select
     python train.py group=$1_rota model=unet levels=1 kernel_size=0 k_select=$k_select rotation=180
     python train.py group=$1_rota model=unet levels=0 kernel_size=0 k_select=$k_select rotation=180
     python train.py group=$1_flips model=unet levels=1 kernel_size=0 k_select=$k_select flips=1
