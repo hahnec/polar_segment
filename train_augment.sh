20039d38ac63f6e76f4414998ca5c348d5146342
hahnec <inbox@christopherhahne.de>
2024-10-30 12:41:24 +0100

    style(names): adjust groupnames

--- train_augment.sh
+++ train_augment.sh
@@ -6,8 +6,8 @@ for k_select in {0..11}; do
     python train.py group=$1_absence model=unet levels=0 kernel_size=0 k_select=$k_select
     python train.py group=$1_rota model=unet levels=1 kernel_size=0 k_select=$k_select rotation=180
     python train.py group=$1_rota model=unet levels=0 kernel_size=0 k_select=$k_select rotation=180
-    python train.py group=$1_flips model=unet levels=1 kernel_size=0 k_select=$k_select flips=1
-    python train.py group=$1_flips model=unet levels=0 kernel_size=0 k_select=$k_select flips=1
-    python train.py group=$1_rotaflips model=unet levels=1 kernel_size=0 k_select=$k_select rotation=180 flips=1
-    python train.py group=$1_rotaflips model=unet levels=0 kernel_size=0 k_select=$k_select rotation=180 flips=1
+    python train.py group=$1_flip model=unet levels=1 kernel_size=0 k_select=$k_select flips=1
+    python train.py group=$1_flip model=unet levels=0 kernel_size=0 k_select=$k_select flips=1
+    python train.py group=$1_rotaflip model=unet levels=1 kernel_size=0 k_select=$k_select rotation=180 flips=1
+    python train.py group=$1_rotaflip model=unet levels=0 kernel_size=0 k_select=$k_select rotation=180 flips=1
 done
