f277aa066c2faf88209fe7775d6657889adbedea
hahnec <inbox@christopherhahne.de>
2024-11-04 18:16:48 +0100

    refactor(ce-loss): revert to cross-entropy

--- train.py
+++ train.py
@@ -96,10 +96,10 @@ def batch_iter(frames, truth, cfg, model, train_opt=0, criterion=None, optimizer
 def epoch_iter(cfg, dataloader, model, mm_model=None, branch_type='test', step=None, log_img=False, epoch=None, optimizer=None, grad_scaler=None):
 
     from utils.dice_loss import DiceLoss
-    criterion = DiceLoss() #torch.nn.CrossEntropyLoss() #(lambda x, y: sigmoid_focal_loss_multiclass(x, y, alpha=cfg.alpha, gamma=cfg.gamma).mean()) if branch_type != 'test' else None
+    criterion = torch.nn.CrossEntropyLoss() #(lambda x, y: sigmoid_focal_loss_multiclass(x, y, alpha=cfg.alpha, gamma=cfg.gamma).mean()) if branch_type != 'test' else None
     if cfg.class_num > 3 and branch_type != 'test':
         from utils.multi_loss import multi_loss_aggregation
-        criterion = criterion = DiceLoss() #torch.nn.CrossEntropyLoss() #lambda x, y: multi_loss_aggregation(x, y, loss_fun=lambda x, y: sigmoid_focal_loss_multiclass(x, y, alpha=cfg.alpha, gamma=cfg.gamma).mean())
+        criterion = criterion = torch.nn.CrossEntropyLoss() #lambda x, y: multi_loss_aggregation(x, y, loss_fun=lambda x, y: sigmoid_focal_loss_multiclass(x, y, alpha=cfg.alpha, gamma=cfg.gamma).mean())
     train_opt = 0 if optimizer is None else 1
     model.train() if train_opt else model.eval()
     batch_it = lambda f, t: batch_iter(f, t, cfg=cfg, model=model, train_opt=train_opt, criterion=criterion, optimizer=optimizer, grad_scaler=grad_scaler)
