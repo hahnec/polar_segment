d60af06134e0aeba0a6753106de81505bf4b62f0
Hahne Christopher (ch21z139) <inbox@christopherhahne.de>
2024-11-17 16:20:33 +0100

    chore(gamma): skip gamma augmentation

--- train_augment.sh
+++ train_augment.sh
@@ -12,5 +12,5 @@ for k_select in {0..3}; do
     python train.py group=$1_rotaflip model=unet levels=0 kernel_size=0 k_select=$k_select rotation=180 flips=1
 #    python train.py group=$1_crop model=unet levels=0 kernel_size=0 k_select=$k_select crop=1
     python train.py group=$1_noise model=unet levels=0 kernel_size=0 k_select=$k_select noise=0.1
-    python train.py group=$1_gamma model=unet levels=0 kernel_size=0 k_select=$k_select gamma=1
+#    python train.py group=$1_gamma model=unet levels=0 kernel_size=0 k_select=$k_select gamma=1
 done
