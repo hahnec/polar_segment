16711827109828a0ae732d267d93ffc4c64bce5a
hahnec <inbox@christopherhahne.de>
2025-01-27 08:30:56 +0100

    chore(loss): apply mask after loss loss computation

--- train.py
+++ train.py
@@ -62,6 +62,7 @@ def batch_iter(frames, truth, cfg, model, train_opt=0, criterion=None, optimizer
         m = (m.float() * mask).bool()
 
     # inference
+    t_s = torch.tensor([float('NaN')])
     if not train_opt:
         start = torch.cuda.Event(enable_timing=True)
         end = torch.cuda.Event(enable_timing=True)
@@ -70,13 +71,13 @@ def batch_iter(frames, truth, cfg, model, train_opt=0, criterion=None, optimizer
     if not train_opt:
         end.record()
         torch.cuda.synchronize()
-    t_s = start.elapsed_time(end) / 1000 if not train_opt else torch.tensor([float('NaN')])
+        t_s = start.elapsed_time(end) / 1000
 
     # loss and back-propagation
     loss = None
     if criterion and preds.numel() > 0:
-        loss = criterion(preds*m, truth*m)
-        #loss = loss * m.squeeze(1)
+        loss = criterion(preds, truth)
+        loss = loss * m.squeeze(1)
         loss = loss.sum() #/ (m.sum() + 1e-8)
     if train_opt and loss is not None and torch.isfinite(m).all() and m.any(): 
         optimizer.zero_grad(set_to_none=True)
