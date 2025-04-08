fix(loss): average pixel loss by default instead of summing pixel loss

train.py
@@ -76,8 +76,6 @@ def batch_iter(frames, truth, cfg, model, train_opt=0, criterion=None, optimizer
     loss = None
     if criterion and preds.numel() > 0:
         loss = criterion(preds*m, truth*m)
-        #loss = loss * m.squeeze(1)
-        loss = loss.sum() #/ (m.sum() + 1e-8)
     if train_opt and loss is not None and torch.isfinite(m).all() and m.any(): 
         optimizer.zero_grad(set_to_none=True)
         loss.backward()
@@ -104,10 +102,10 @@ def batch_iter(frames, truth, cfg, model, train_opt=0, criterion=None, optimizer
 
 def epoch_iter(cfg, dataloader, model, mm_model=None, branch_type='test', step=None, log_img=False, epoch=None, optimizer=None):
 
-    criterion = torch.nn.CrossEntropyLoss() if not cfg.imbalance or branch_type == 'test' else (lambda x, y: sigmoid_focal_loss_multiclass(x, y, alpha=cfg.alpha, gamma=cfg.gamma, reduction='none'))
+    criterion = torch.nn.CrossEntropyLoss() if not cfg.imbalance or branch_type == 'test' else (lambda x, y: sigmoid_focal_loss_multiclass(x, y, alpha=cfg.alpha, gamma=cfg.gamma))
     if cfg.class_num > 3 and branch_type != 'test':
         from utils.multi_loss import multi_loss_aggregation
-        criterion = torch.nn.CrossEntropyLoss(reduction='none') if not cfg.imbalance or branch_type == 'test' else (lambda x, y: sigmoid_focal_loss_multiclass(x, y, alpha=cfg.alpha, gamma=cfg.gamma, reduction='none'))
+        criterion = torch.nn.CrossEntropyLoss(reduction='none') if not cfg.imbalance or branch_type == 'test' else (lambda x, y: sigmoid_focal_loss_multiclass(x, y, alpha=cfg.alpha, gamma=cfg.gamma))
     train_opt = 0 if optimizer is None else 1
     model.train() if train_opt else model.eval()
     batch_it = lambda f, t: batch_iter(f, t, cfg=cfg, model=model, train_opt=train_opt, criterion=criterion, optimizer=optimizer)