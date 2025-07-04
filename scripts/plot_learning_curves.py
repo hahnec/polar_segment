fix(augment): use smaller rotation range and enable 7th graph style

scripts/plot_learning_curves.py
@@ -31,7 +31,7 @@ def plot_curves(curve_means, curve_stds, labels=None, filename='', fontsize=18, 
     plt.figure(figsize=(15, 15))
     fig, axs = plt.subplots(1, 1)
     x = np.arange(len(curve_means[0]))
-    colors = ['#d62728', '#8c564b', '#1f77b4', '#ff7f0e', '#2ca02c', '#9467bd'][:len(curve_means)]
+    colors = ['#d62728', '#8c564b', '#1f77b4', '#ff7f0e', '#2ca02c', '#9467bd', '#777777'][:len(curve_means)]
     styles = ['-', '-.', (0, (3, 1, 1, 1, 1, 1)), (5, (10, 3)), '--', ':', (0, (3, 5, 1, 5, 1, 5))][:len(curve_means)]
     labels = [str(num) for num in range(len(curve_means))] if labels is None else labels
     for mean, std, l, c, s in zip(curve_means, curve_stds, labels, colors, styles):
@@ -67,7 +67,7 @@ def plot_curves(curve_means, curve_stds, labels=None, filename='', fontsize=18, 
 
     axs.set_xlabel('Steps [\\#]', fontsize=fontsize)
     axs.set_ylabel(filename.split('_')[-1].capitalize()+' [a.u.]', fontsize=fontsize)
-    axs.legend(loc='upper left' if filename.__contains__('loss') else 'lower right', fontsize=fontsize)
+    axs.legend(loc='upper right' if filename.__contains__('loss') else 'lower right', fontsize=fontsize)
     plt.tight_layout()
     plt.savefig(Path(__file__).parent / (filename+'_figure.svg'), format='svg')
 