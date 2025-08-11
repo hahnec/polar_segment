3fbcb4283d389696259ef922fc82f8b7f6ffc6ed
Hahne Christopher (ch21z139) <inbox@christopherhahne.de>
2025-08-11 11:49:21 +0200

    chore(unet): use only unet

--- train_stack.sh
+++ train_stack.sh
@@ -2,11 +2,11 @@
 
 for k_select in {0..2}; do
     # MLP
-    python train.py group=$1 model=mlp levels=1 kernel_size=0 k_select=$k_select
-    python train.py group=$1 model=mlp levels=0 kernel_size=0 k_select=$k_select
+    #python train.py group=$1 model=mlp levels=1 kernel_size=0 k_select=$k_select
+    #python train.py group=$1 model=mlp levels=0 kernel_size=0 k_select=$k_select
     # resnet
-    python train.py group=$1 model=resnet levels=1 kernel_size=0 k_select=$k_select
-    python train.py group=$1 model=resnet levels=0 kernel_size=0 k_select=$k_select
+    #python train.py group=$1 model=resnet levels=1 kernel_size=0 k_select=$k_select
+    #python train.py group=$1 model=resnet levels=0 kernel_size=0 k_select=$k_select
     # U-net
     python train.py group=$1 model=unet levels=1 kernel_size=0 k_select=$k_select
     python train.py group=$1 model=unet levels=0 kernel_size=0 k_select=$k_select
