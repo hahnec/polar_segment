61444253870fba07b9462160e98dbfb99e215a27
hahnec <inbox@christopherhahne.de>
2025-08-13 10:24:46 +0200

    fix(dist): improve distribution bundling

--- install.sh
+++ install.sh
@@ -23,10 +23,10 @@ VENV_DIR="venv"
 if [ -d "$VENV_DIR" ]; then
     echo "Directory '$VENV_DIR' already exists. Skipping git clone."
 else
-    python -m pip install pip --upgrade
-    python -m venv venv
+    python3 -m pip install pip --upgrade
+    python3 -m venv venv
 fi
 
 source venv/bin/activate
-python -m pip install -r requirements.txt
-pip install torch==2.4.1 torchvision==0.19.1 --index-url https://download.pytorch.org/whl/cu121
+python3 -m pip install -r requirements.txt
+python3 -m pip install torch==2.4.1 torchvision==0.19.1 --index-url https://download.pytorch.org/whl/cu121
